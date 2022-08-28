ead0d3e9f4cdd2d528f6dc6a46a4e5856eb2a55a
Ali Farrokhtala <farrokhtala@gmail.com>
2022-08-28 15:11:27 -0700

    added cart item count

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,22 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-component.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <dev className='cart-dropdown-container'>
-      <div className='cart-item'></div>
+      <div className='cart-item'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </dev>
   );
