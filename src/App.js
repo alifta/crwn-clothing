692aab750034ee438d26260062248b11e32b8f10
Ali Farrokhtala <farrokhtala@gmail.com>
2022-08-28 18:47:10 -0700

    created checkout page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 import Authentication from './routes/authentication/authentication.component';
 import NoMatch from './components/nomatch/nomatch.component';
-import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element={<Checkout />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='*' element={<NoMatch />} />
       </Route>
